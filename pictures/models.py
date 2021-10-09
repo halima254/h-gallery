6d09c11ba264637a387f4c92549222b48faede82
halima254 <halima.yahya@student.moringaschool.com>
2021-10-09 23:56:10 +0300

    create editor class

--- pictures/models.py
+++ pictures/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Editor(models.Model):
+    #edito class inherits from tje modules.Model class
+    first_name = models.CharField(max_length = 30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=10,blank=True)
+    
+    def __str__(self):
+        return self.first_name
