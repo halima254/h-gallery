ca50f7dc216a4614adfbd7482710ef939d621933
halima254 <halima.yahya@student.moringaschool.com>
2021-10-10 11:09:13 +0300

    create view for pictures, category and location

--- pictures/views.py
+++ pictures/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Editor, Pictures, Location, a
+from .models import Editor, Pictures, Location, Category
 
 # Create your views here.
 def welcome(request):
     return HttpResponse('welcome to my Gallery!')
+
+
+def index(request):
+    pictures = Pictures.all_pictures()
+    category = Category.get_category()
+    location = Location.get_location()
+    
+    return render(request, 'index.html',{'pictures':pictures,'category':category,'location':location})
