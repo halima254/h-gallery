2b268a079daecc1e53cbb083dce68dc558ffbd7d
halima254 <halima.yahya@student.moringaschool.com>
2021-10-10 12:40:25 +0300

    install pillow

--- pictures/urls.py
+++ pictures/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import url
-from . import  views
+from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    url(r'^$', view.index,name='index'),
-    url(r'^location/(\d+)', view.pictures_location,name='location'),
-    url(r'^category/(\d+)', view.pictures_category,name='category'),
-    url(r'^single_pic/(\d+)', view.single_picture,name='single_pic'),
-    url(r'^search/(\d+)', view.search_picture,name='search_results'),
+    url(r'^$', views.index,name='index'),
+    url(r'^location/(\d+)', views.pictures_location,name='location'),
+    url(r'^category/(\d+)', views.pictures_category,name='category'),
+    url(r'^single_pic/(\d+)', views.single_picture,name='single_pic'),
+    url(r'^search/(\d+)', views.search_picture,name='search_results'),
 ]
 
 if settings.DEBUG:
