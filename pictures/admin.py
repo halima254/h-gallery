999315504b0aef954a65451eb4145bb22a7a5f5d
halima254 <halima.yahya@student.moringaschool.com>
2021-10-10 12:20:40 +0300

    register classes to admin

--- pictures/admin.py
+++ pictures/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Category, Editor, Pictures, Location
+
+admin.site.register(Editor)
+admin.site.register(Location)
+admin.site.register(Category)
+admin.site.register(Pictures)
